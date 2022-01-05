d64c12a8eeb5e952cc83975729a2e0116599c6d7
Zachary J. Fields <zachary_fields@yahoo.com>
2022-01-05 12:10:49 -0600

    chore: merge CI with existing Dockerfile

--- cmake-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo Running CMake
-[ -d cmake-build-debug ] || mkdir cmake-build-debug
-cd cmake-build-debug
-cmake -DCMAKE_TOOLCHAIN_FILE=../arm-gcc-toolchain.cmake -DCMAKE_BUILD_TYPE=Debug .. && cmake --build . -- -j 4
