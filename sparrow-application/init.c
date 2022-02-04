8800df87dd71238772f63d31d11d215ed59c6481
Zachary J. Fields <zachary_fields@yahoo.com>
2022-02-04 14:56:28 +0000

    chore: Load BME and PIR apps by default

--- sparrow-application/init.c
+++ sparrow-application/init.c
@@ -6,9 +6,22 @@
 #include <framework.h>
 
 // Scheduled App Header(s)
+#include "bme/bme.h"
 #include "diag/diag.h"
+#include "pir/pir.h"
 
 void schedAppInit (void) {
+    // Will automatically disable if BME280 is not detected
+    if (bmeInit()) {
+        APP_PRINTF("ERROR: Failed to initialize BME280 application!\r\n");
+    }
+
+    // Will automatically disable if BME application is not detected
+    if (pirInit()) {
+        APP_PRINTF("ERROR: Failed to initialize PIR application!\r\n");
+    }
+
+    // Reports memory usage at timed interval or on button click
     if (diagInit()) {
         APP_PRINTF("ERROR: Failed to initialize diagnostic application!\r\n");
     }
